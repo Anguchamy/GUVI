7f489879ec6370dbe23c806eb860d036b01ef643
Anguchamy <41102412+Anguchamy@users.noreply.github.com>
2018-12-02 12:45:56 +0530

    Add files via upload

---
+++ IndexOfTheString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass 
+{
+    public static void main(String args[]) 
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String b=s.nextLine();
+        System.out.println(a.indexOf(b));
+    }
+}
